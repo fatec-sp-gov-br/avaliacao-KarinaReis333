f43e5943cbc5209dbbfb3b34c948664e94fa37dc
KarinaReis333 <karina.reis24@gmail.com>
2026-06-18 18:25:48 -0300

    Refactor Personagem class and fix method implementations

--- src/main/java/br/edu/poo/Personagem.java
+++ src/main/java/br/edu/poo/Personagem.java
@@ -1,35 +1,55 @@
 package br.edu.poo;
 
 public class Personagem {
+    // Atributos (Adicionado 'final' para limpar os avisos do VS Code)
+    private final String nome;
+    private int vida; // Não pode ser final porque muda no combate
+    private final int ataque;
+    private final int defesa;
+    private boolean pocaoUtilizada;
 
-    public Personagem(String nome,
-                      int vida,
-                      int ataque,
-                      int defesa) {
-
+    // Construtor
+    public Personagem(String nome, int vida, int ataque, int defesa) {
+        this.nome = nome;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defesa = defesa;
+        this.pocaoUtilizada = false;
     }
 
+    // Métodos obrigatórios
     public void atacar(Personagem inimigo) {
-
+        int danoCalculado = this.ataque - inimigo.defesa;
+        if (danoCalculado < 1) {
+            danoCalculado = 1;
+        }
+        inimigo.receberDano(danoCalculado);
     }
 
     public void receberDano(int dano) {
-
+        this.vida -= dano;
+        if (this.vida < 0) {
+            this.vida = 0;
+        }
     }
 
     public void usarPocao(Pocao pocao) {
-
+        if (!this.pocaoUtilizada) {
+            this.vida += pocao.getCura();
+            this.pocaoUtilizada = true;
+            System.out.println(this.nome + " usa " + pocao.getNome());
+        }
     }
 
     public boolean estaVivo() {
-        return false;
+        return this.vida > 0;
     }
 
     public String getNome() {
-        return null;
+        return this.nome;
     }
 
     public int getVida() {
-        return 0;
+        return this.vida;
     }
 }
