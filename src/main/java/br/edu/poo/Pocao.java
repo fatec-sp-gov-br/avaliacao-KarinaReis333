ddbb8f3bb6e14aa7afa02a413c1e97e01de3fe1f
KarinaReis333 <karina.reis24@gmail.com>
2026-06-18 18:34:24 -0300

    Update Pocao.java

--- src/main/java/br/edu/poo/Pocao.java
+++ src/main/java/br/edu/poo/Pocao.java
@@ -1,16 +1,22 @@
 package br.edu.poo;
 
 public class Pocao {
+    // Atributos
+    private final String nome;
+    private final int cura;
 
+    // Construtor
     public Pocao(String nome, int cura) {
-
+        this.nome = nome;
+        this.cura = cura;
     }
 
+    // Métodos
     public String getNome() {
-        return null;
+        return this.nome;
     }
 
     public int getCura() {
-        return 0;
+        return this.cura;
     }
 }
